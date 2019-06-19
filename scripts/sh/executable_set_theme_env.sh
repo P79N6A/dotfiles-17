31298d10c9b002a8774befe2e38ed2e3f7020d7a
Steven Pilger <stevenpilger@fastmail.com>
2019-06-19 09:36:36 +0200

    fixing error with time comparison

--- scripts/sh/executable_set_theme_env.sh
+++ scripts/sh/executable_set_theme_env.sh
@@ -4,10 +4,10 @@ now=$(date +"%H")
 lwd=$pwd
 
 cd ~/.config/kitty/
-if (( $now >= 19 )); then
+if (( 10#$now >= 19 )); then
 	echo 'Setting dark theme...'
 	cat onehalfdark.conf > current_theme.conf
-elif (( $now >= 8 )); then
+elif (( 10#$now >= 8 )); then
 	echo 'Setting light theme...'
 	cat onehalflight.conf > current_theme.conf
 else cat onehalfdark.conf > current_theme.conf
