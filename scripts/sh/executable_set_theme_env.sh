31d1a4e91c036596b88ddb96cfa3c1ffa3568c13
Steven Pilger <stevenpilger@fastmail.com>
2019-06-20 10:55:35 +0200

    new logic added to export tmuxline style from cron plus 'jk' remapped to esc in vim

--- scripts/sh/executable_set_theme_env.sh
+++ scripts/sh/executable_set_theme_env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# getting current time and defaul working directory
 now=$(date +"%H")
 lwd=$pwd
 
+# setting the kitty theme appropriately
 cd ~/.config/kitty/
 if (( 10#$now >= 19 )); then
 	echo 'Setting dark theme...'
@@ -13,4 +15,15 @@ elif (( 10#$now >= 8 )); then
 else cat onehalfdark.conf > current_theme.conf
 fi
 
-cd $pwd
+# setting the tmuxline theme appropriately
+cd ~
+if (( 10#$now >= 19 )); then
+	echo 'Setting dark theme...'
+	cat .tmuxline.onehalfdark.conf > .tmuxline.conf
+elif (( 10#$now >= 8 )); then
+	echo 'Setting light theme...'
+	cat .tmuxline.onehalflight.conf > .tmuxline.conf
+else cat .tmuxline.onehalfdark.conf > .tmuxline.conf
+fi
+
+cd $lwd
